b4ec926b91c236c43f03d7c30a461b7472e90c5f
Martín Settimo <martinsettimo@hotmail.com>
2026-01-29 00:19:52 -0300

    otras modificaciones

--- saldo.js
+++ saldo.js
@@ -2,10 +2,6 @@ let saldo =980000;
 let tipoDeCambio=1475;
 const montoPrestamo="$250.000  plazo:6 años 38% T.N.A"
  
-function comprarDolares(){
-    let montoIngresado =document.getElementById("dolares").value;
-    const equivalente=montoIngresado/tipoDeCambio;
-    if(montoIngresado>=saldo) {
 
 
 
